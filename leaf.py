17454fe73daa1862dd0258c3a3f0cafe9d1bfa39
Fred C <github-fred@hidzz.com>
2018-08-21 17:11:21 -0700

    Fix indentation Add colors Add x labels

--- leaf.py
+++ leaf.py
@@ -16,6 +16,7 @@ import os
 import sys
 
 from datetime import datetime
+from functools import partial
 
 import json
 import matplotlib.pyplot as plt
@@ -38,140 +39,151 @@ class Config(object):
 
 
 class WsprData(object):
-    __slot__ = [
-        "distance", "tx_call", "timestamp", "drift", "tx_grid", "rx_call", "power_dbm",
-        "rx_grid", "azimuth", "snr", "freq",
-    ]
+  __slot__ = [
+    "distance", "tx_call", "timestamp", "drift", "tx_grid", "rx_call", "power_dbm",
+    "rx_grid", "azimuth", "snr", "freq",
+  ]
 
-    def __init__(self, *args, **kwargs):
-        for key, val, in kwargs.items():
-            if key == 'timestamp':
-                val = datetime.utcfromtimestamp(val)
-            setattr(self, key, val)
+  def __init__(self, *args, **kwargs):
+    for key, val, in kwargs.items():
+      setattr(self, key, val)
 
 
 def readfile(filename):
-    try:
-      with open(filename, 'rb') as fdi:
-        data = json.load(fdi)
-    except (ValueError, IOError) as err:
-      logging.error(err)
-      sys.exit(os.EX_OSFILE)
+  try:
+    with open(filename, 'rb') as fdi:
+      data = json.load(fdi)
+  except (ValueError, IOError) as err:
+    logging.error(err)
+    sys.exit(os.EX_OSFILE)
 
-    return [WsprData(**d) for d in data]
+  return [WsprData(**d) for d in data]
 
 def download():
-    params = dict(
-        callsign=DXPLORER_CALL,
-        key=DXPLORER_URL,
-        timelimit=Config.timelimit,
-    )
-    resp = requests.get(url=DXPLORER_URL, params=params)
-    data = resp.json()
-    logging.info('Downloaded %d records', len(data))
-    return [WsprData(**d) for d in data]
+  params = dict(
+    callsign=DXPLORER_CALL,
+    key=DXPLORER_URL,
+    timelimit=Config.timelimit,
+  )
+  resp = requests.get(url=DXPLORER_URL, params=params)
+  data = resp.json()
+  logging.info('Downloaded %d records', len(data))
+  return [WsprData(**d) for d in data]
 
 def reject_outliers(data, magnitude=1.2):
-    q25, q75 = np.percentile(data, [25, 75])
-    iqr = q75 - q25
+  q25, q75 = np.percentile(data, [25, 75])
+  iqr = q75 - q25
 
-    min = q25 - (iqr * magnitude)
-    max = q75 + (iqr * magnitude)
+  min = q25 - (iqr * magnitude)
+  max = q75 + (iqr * magnitude)
 
-    return [x for x in data if min <= x <= max]
+  return [x for x in data if min <= x <= max]
 
 def azimuth(wspr_data):
-    filename = os.path.join(Config.target, 'azimuth.png')
-    logging.info('Drawing azimuth to %s', filename)
-    data = collections.defaultdict(set)
-    for node in wspr_data:
-      key = Config.granularity * (node.azimuth / Config.granularity)
-      data[key].add(node.distance)
+  filename = os.path.join(Config.target, 'azimuth.png')
+  logging.info('Drawing azimuth to %s', filename)
 
-    elements = []
-    for azim, dists in data.iteritems():
-      for dist in reject_outliers(list(dists)):
-        elements.append((azim, dist))
+  data = collections.defaultdict(set)
+  for node in wspr_data:
+    key = Config.granularity * (node.azimuth / Config.granularity)
+    data[key].add(node.distance)
 
-    az, el = zip(*elements)
+  elements = []
+  for azim, dists in data.iteritems():
+    for dist in reject_outliers(list(dists)):
+      elements.append((azim, dist))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, polar=True)
-    ax.set_theta_zero_location("N")
-    ax.set_theta_direction(-1)
+  az, el = zip(*elements)
 
-    ax.scatter(az, el)
-    ax.set_title('Azimuth\nDistance', loc='left')
-    plt.savefig(filename)
-    plt.close()
+  fig = plt.figure()
+  ax = fig.add_subplot(111, polar=True)
+  ax.set_theta_zero_location("N")
+  ax.set_theta_direction(-1)
 
+  ax.scatter(az, el)
+  ax.set_title('Azimuth\nDistance', loc='left')
+  plt.savefig(filename)
+  plt.close()
 
-def boxPlot(data):
-    # basic plot
-    filename = os.path.join(Config.target, 'boxplot.png')
-    logging.info('Drawing boxplot to %s', filename)
-    fig, ax = plt.subplots(figsize=Config.fig_size)
-    ax.boxplot(data)
-    ax.grid(True)
+def boxPlot(wspr_data):
+  # basic plot
+  filename = os.path.join(Config.target, 'boxplot.png')
+  logging.info('Drawing boxplot to %s', filename)
 
-    plt.title('Distances')
-    plt.grid(linestyle='dotted')
-    plt.savefig(filename)
-    plt.close()
+  collection = collections.defaultdict(list)
+  for val in wspr_data:
+    key = 3600 * (val.timestamp / 3600)
+    collection[key].append(val.distance)
 
-def violinPlot(data):
-    filename = os.path.join(Config.target, 'violin.png')
-    logging.info('Drawing violin to %s', filename)
-    # get only the relevant data
-    values = []
-    for val in data:
-        values.append(reject_outliers(val))
+  data = []
+  for key, values in sorted(collection.items()):
+    data.append((datetime.utcfromtimestamp(key), values))
 
-    fig, ax = plt.subplots(figsize=Config.fig_size)
-    ax.violinplot(values, showmeans=False, showmedians=True)
-    ax.grid(True)
+  hours, values = zip(*data)
+  fig, ax = plt.subplots(figsize=Config.fig_size)
+  bplot = ax.boxplot(values, sym="b.", patch_artist=True)
+  for patch in bplot['boxes']:
+    patch.set(color='b', linewidth=1)
 
-    plt.title('Distances')
-    plt.grid(linestyle='dotted')
-    plt.savefig(filename)
-    plt.close()
+    patch.set_color('lightblue')
+
+  ax.grid(True)
+  ax.set_xticklabels(['{}'.format(h.strftime('%d %R')) for h in hours])
+
+  plt.title('Distances')
+  plt.grid(linestyle='dotted')
+  plt.savefig(filename)
+  plt.close()
+
+def violinPlot(wspr_data):
+  filename = os.path.join(Config.target, 'violin.png')
+  logging.info('Drawing violin to %s', filename)
+
+  # get only the relevant data and reject the outliers
+  collection = collections.defaultdict(list)
+  for val in wspr_data:
+    key = 3600 * (val.timestamp / 3600)
+    collection[key].append(val.distance)
+
+  data = []
+  for key, values in sorted(collection.items()):
+    data.append((datetime.utcfromtimestamp(key), reject_outliers(values)))
+
+  hours, values = zip(*data)
+  fig, ax = plt.subplots(figsize=Config.fig_size)
+  ax.grid(True)
+  ax.set_xticklabels(['{}'.format(h.strftime('%d %R')) for h in hours])
+
+  ax.violinplot(values, showmeans=False, showmedians=True)
+
+  plt.title('Distances')
+  plt.grid(linestyle='dotted')
+  plt.savefig(filename)
+  plt.close()
 
 def main():
-    parser = argparse.ArgumentParser(description='WSPR Stats.')
-    parser.add_argument('-d', '--debug', action='store_true', default=False,
-                        help='Print information useful for debugging')
-    parser.add_argument('-t', '--target-dir', default='/tmp',
-                        help=('Target directory where the images will be '
-                              'saved [default: %(default)s]'))
-    parser.add_argument('-f', '--file', help='JSON file from DXPlorer.net')
-    pargs = parser.parse_args()
-    Config.target = pargs.target_dir
-    if pargs.debug:
-      _logger = logging.getLogger()
-      _logger.setLevel('DEBUG')
-      del _logger
+  parser = argparse.ArgumentParser(description='WSPR Stats.')
+  parser.add_argument('-d', '--debug', action='store_true', default=False,
+                      help='Print information useful for debugging')
+  parser.add_argument('-t', '--target-dir', default='/tmp',
+                      help=('Target directory where the images will be '
+                            'saved [default: %(default)s]'))
+  parser.add_argument('-f', '--file', help='JSON file from DXPlorer.net')
+  pargs = parser.parse_args()
+  Config.target = pargs.target_dir
+  if pargs.debug:
+    _logger = logging.getLogger()
+    _logger.setLevel('DEBUG')
+    del _logger
 
-    if pargs.file:
-      wspr_data = readfile(pargs.file)
-    else:
-      wspr_data = download()
+  if pargs.file:
+    wspr_data = readfile(pargs.file)
+  else:
+    wspr_data = download()
 
-    collection = collections.defaultdict(list)
-    for val in wspr_data:
-        key = val.timestamp.day * 100 + val.timestamp.hour
-        collection[key].append(val.distance)
-
-    values = [np.array(v[1]) for v in sorted(collection.items())]
-
-    boxPlot(values)
-    violinPlot(values)
-    azimuth(wspr_data)
+  boxPlot(wspr_data)
+  violinPlot(wspr_data)
+  azimuth(wspr_data)
 
 if __name__ == "__main__":
-    main()
-
-# Local Variables:
-# python-indent-offset: 4
-# tab-width: 4
-# python-indent: 4
-# End:
+  main()
